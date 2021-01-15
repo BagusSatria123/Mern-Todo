3fce51107b731aaca59f5e568174d0dd648d740d
BagusSatria123 <satriabagusputra123@gmail.com>
2021-01-15 21:42:54 +0700

    Backend First API

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express,{Request,Response} from 'express'
+
+const app = express()
+const PORT = 8080
+
+app.get('/',(req: Request,res: Response)=>{
+    res.send('Hello, World!')
+})
+
+app.get('/about',(req: Request,res: Response)=>{
+    res.send('This is about route')
+})
+
+app.listen(PORT, ()=>{
+    console.info(`Example app listening at http://localhost:${PORT}`)
+})
