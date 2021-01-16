f67c25c9618e13c2951f3196f42a74e38082f4d1
BagusSatria123 <satriabagusputra123@gmail.com>
2021-01-16 21:04:20 +0700

    Backend 10 Connect MongoDB Atlas with Express Server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,21 @@
 import express,{Request,Response} from 'express'
+import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express()
 const PORT = 8080
+
+const {
+    MONGODB_ATLAS_USERNAME,
+    MONGODB_ATLAS_PASSWORD,
+    MONGODB_ATLAS_DBNAME
+} = process.env
+
+const uri = `mongodb+srv://${MONGODB_ATLAS_USERNAME}:${MONGODB_ATLAS_PASSWORD}@cluster0.k5rmy.mongodb.net/${MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+app.use(cors());
+
 
 app.get('/',(req: Request,res: Response)=>{
     res.send('Hello, World!')
@@ -11,6 +25,14 @@ app.get('/about',(req: Request,res: Response)=>{
     res.send('This is about route')
 })
 
-app.listen(PORT, ()=>{
-    console.info(`Example app listening at http://localhost:${PORT}`)
-})
+mongoose.set('useFindAndModify', true);
+mongoose.connect(uri, options)
+    .then(() => {
+        app.listen(PORT, () =>{
+            console.info(`Example app listening at http://localhost:${PORT}`) 
+        })
+    })
+    .catch((error) =>{
+        throw error
+    })
+
